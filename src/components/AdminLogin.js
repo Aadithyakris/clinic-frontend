f4493901105d1cdce5b21c65359b0f876a5131a0
Aadithyakris <aadikris123@gmail.com>
2025-06-29 17:05:23 +0530

    ui-slot-generate

--- src/components/AdminLogin.js
+++ src/components/AdminLogin.js
@@ -1,52 +1,5 @@
-// // src/components/AdminLogin.js
-// import { useState } from 'react';
-
-// const AdminLogin = ({ onLogin }) => {
-//   const [password, setPassword] = useState('');
-//   const [error, setError] = useState('');
-//   const [showPassword, setShowPassword] = useState(false);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     // Change this to your desired admin password
-//     if (password === 'doctor123') {
-//       onLogin();
-//     } else {
-//       setError('Incorrect password');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h2>Admin Login</h2>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type={showPassword ? 'text' : 'password'}
-//           placeholder="Enter admin password"
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//         />
-//         <label>
-//           <input
-//             type="checkbox"
-//             checked={showPassword}
-//             onChange={() => setShowPassword((prev) => !prev)}
-//           />
-//           Show password
-//         </label>
-//         <br />
-//         <button type="submit">Login</button>
-//       </form>
-//       {error && <p style={{ color: 'red' }}>{error}</p>}
-//     </div>
-//   );
-// };
-
-// export default AdminLogin;
-
-
 // src/components/AdminLogin.js
+
 import { useState } from 'react';
 
 const AdminLogin = ({ onLogin }) => {
